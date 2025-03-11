d15f592edc7b5cab61ede9f6a887a8e7fe98fe09
operatios <23556388+operatios@users.noreply.github.com>
2025-03-11 05:55:03 +0300

    Add set-screenshot-dir

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mpv-scripts
+
+### [set-screenshot-dir](set-screenshot-dir.lua)
+Sets `screenshot-dir` to the standard Windows folder (`%USERPROFILE%/Pictures/Screenshots/mpv`) using [`SHGetKnownFolderPath`](https://learn.microsoft.com/en-us/windows/win32/api/shlobj_core/nf-shlobj_core-shgetknownfolderpath). Useful if `Pictures` or `Screenshots` folder location has been changed.
---
+++ set-screenshot-dir.lua
@@ -0,0 +1,46 @@
+local ffi = require "ffi"
+
+local Shell32 = ffi.load("Shell32") -- SHGetKnownFolderPath
+local Ole32 = ffi.load("Ole32") -- CoTaskMemFree
+
+ffi.cdef[[
+    void* malloc(size_t);
+    void free(void*);
+
+    typedef struct {
+        unsigned long Data1;
+        unsigned short Data2;
+        unsigned short Data3;
+        unsigned char Data4[8];
+    } GUID;
+    void CoTaskMemFree(void*);
+    long SHGetKnownFolderPath(GUID*, unsigned long, void*, wchar_t**);
+    int WideCharToMultiByte(unsigned int, unsigned long, wchar_t const*, int, char*, int, char const*, bool*);
+]]
+
+FOLDERID_Screenshots = ffi.new("GUID", {0xb7bede81, 0xdf94, 0x4682, {0xa7, 0xd8, 0x57, 0xa5, 0x26, 0x20, 0xb8, 0x6f}})
+
+function utf16_to_utf8(wstr)
+    local size = ffi.C.WideCharToMultiByte(65001, 0, wstr[0], -1, nil, 0, nil, nil)
+    local str = ffi.gc(ffi.C.malloc(size), ffi.C.free)
+    ffi.C.WideCharToMultiByte(65001, 0, wstr[0], -1, str, size, nil, nil)
+
+    return ffi.string(str)
+end
+
+function get_known_folder_path(guid)
+    local path = ffi.new("wchar_t*[1]")
+    Shell32.SHGetKnownFolderPath(guid, 0, nil, path)
+
+    local utf8_path = utf16_to_utf8(path)
+    Ole32.CoTaskMemFree(path[0])
+
+    return utf8_path
+end
+
+function set_screenshot_dir()
+    local path = get_known_folder_path(FOLDERID_Screenshots):gsub("\\", "/")
+    mp.set_property("screenshot-dir", path .. "/mpv")
+end
+
+mp.register_event("file-loaded", set_screenshot_dir)
